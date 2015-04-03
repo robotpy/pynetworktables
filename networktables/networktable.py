eb1e47f42a341cc35cac636a0016496ecb5c033a
Dustin Spicuzza <dustin@virtualroadside.com>
2015-04-02 23:27:27 -0400

    Disallow common operations on AutoUpdateValue to reduce confusion

--- networktables/networktable.py
+++ networktables/networktable.py
@@ -148,12 +148,42 @@ class AutoUpdateValue:
     of this.
     """
     
+    __slots__ = ['__value']
+    
     def __init__(self, default):
-        self.value = default
+        self.__value = default
         
     def get(self):
         '''Returns the value held by this object'''
-        return self.value
+        return self.__value
+    
+    @property
+    def value(self):
+        return self.__value
+    
+    # Comparison operators et al
+    
+    def __lt__(self, other):
+        raise TypeError("< not allowed on AutoUpdateValue objects. Use the .value attribute instead")
+    def __le__(self, other):
+        raise TypeError("<= not allowed on AutoUpdateValue objects. Use the .value attribute instead")
+    def __eq__(self, other):
+        raise TypeError("== not allowed on AutoUpdateValue objects. Use the .value attribute instead")
+    def __ne__(self, other):
+        raise TypeError("!= not allowed on AutoUpdateValue objects. Use the .value attribute instead")
+    def __gt__(self, other):
+        raise TypeError("> not allowed on AutoUpdateValue objects. Use the .value attribute instead")
+    def __ge__(self, other):
+        raise TypeError(">= not allowed on AutoUpdateValue objects. Use the .value attribute instead")
+    
+    def __bool__(self):
+        raise TypeError("< not allowed on AutoUpdateValue objects. Use the .value attribute instead")
+    
+    def __hash__(self):
+        raise TypeError("__hash__ not allowed on AutoUpdateValue objects")
+    
+    def __repr__(self):
+        return '<AutoUpdateValue: %s>' % (self.__value.__repr__(), )
 
 class AutoUpdateListener:
     
