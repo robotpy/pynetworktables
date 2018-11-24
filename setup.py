9c1c32f76b25723b85bd167ec742446af1fbf0af
David Vo <auscompgeek@users.noreply.github.com>
2018-11-24 19:52:07 +1100

    Actually mark as Python 3.5+
    Stop marking wheels as universal - we're not supporting Python 2 now.

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     packages=find_packages(exclude='tests'),
     install_requires=install_requires,
     extras_require=extras,
-    python_requires='!=3.0.*, !=3.1.*, !=3.2.*',
+    python_requires='>=3.5',
     license="BSD-3-Clause",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
