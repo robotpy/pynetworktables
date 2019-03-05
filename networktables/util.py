20ee015e462fd47c914d27a19c594d0e24e585f2
David Vo <auscompgeek@users.noreply.github.com>
2019-03-05 19:05:36 +1100

    Type hint networktables.util

--- networktables/util.py
+++ networktables/util.py
@@ -1,12 +1,15 @@
-from .networktables import NetworkTables
+from typing import Callable, Optional, Sequence
 
 from ntcore.value import Value
+from .instance import NetworkTablesInstance
 
 __all__ = ["ntproperty", "ChooserControl"]
 
+NetworkTables = NetworkTablesInstance.getDefault()
+
 
 class _NtProperty:
-    def __init__(self, key, defaultValue, writeDefault, persistent):
+    def __init__(self, key: str, defaultValue, writeDefault: bool, persistent: bool):
         self.key = key
         self.defaultValue = defaultValue
         self.writeDefault = writeDefault
@@ -34,24 +37,26 @@ class _NtProperty:
         NetworkTables._api.setEntryValueById(self.ntvalue._local_id, self.mkv(value))
 
 
-def ntproperty(key, defaultValue, writeDefault=True, doc=None, persistent=False):
+def ntproperty(
+    key: str,
+    defaultValue,
+    writeDefault: bool = True,
+    doc: str = None,
+    persistent: bool = False,
+) -> property:
     """
         A property that you can add to your classes to access NetworkTables
         variables like a normal variable.
         
         :param key: A full NetworkTables key (eg ``/SmartDashboard/foo``)
-        :type  key: str
         :param defaultValue: Default value to use if not in the table
         :type  defaultValue: any
         :param writeDefault: If True, put the default value to the table,
                              overwriting existing values
-        :type  writeDefault: bool
         :param doc: If given, will be the docstring of the property.
-        :type  doc: str
         :param persistent: If True, persist set values across restarts.
                            *writeDefault* is ignored if this is True.
-        :type  persistent: bool
-        
+
         Example usage::
         
             class Foo(object):
@@ -99,16 +104,19 @@ class ChooserControl(object):
         object over NetworkTables.
     """
 
-    def __init__(self, key, on_choices=None, on_selected=None):
+    def __init__(
+        self,
+        key: str,
+        on_choices: Optional[Callable[[Sequence[str]], None]] = None,
+        on_selected: Optional[Callable[[str], None]] = None,
+    ) -> None:
         """
             :param key: NetworkTables key
-            :type  key: str
             :param on_choices: A function that will be called when the
-                               choices change. Signature: fn(value)
+                               choices change.
             :param on_selection: A function that will be called when the
-                                 selection changes. Signature: fn(value)
+                                 selection changes.
         """
-
         self.subtable = NetworkTables.getTable("SmartDashboard").getSubTable(key)
 
         self.on_choices = on_choices
@@ -117,32 +125,28 @@ class ChooserControl(object):
         if on_choices or on_selected:
             self.subtable.addTableListener(self._on_change, True)
 
-    def close(self):
+    def close(self) -> None:
         """Stops listening for changes to the ``SendableChooser``"""
         if self.on_choices or self.on_selected:
             self.subtable.removeTableListener(self._on_change)
 
-    def getChoices(self):
+    def getChoices(self) -> Sequence[str]:
         """
             Returns the current choices. If the chooser doesn't exist, this
             will return an empty tuple.
-        
-            :rtype: tuple
         """
         return self.subtable.getStringArray("options", ())
 
-    def getSelected(self):
+    def getSelected(self) -> Optional[str]:
         """
             Returns the current selection or None
-        
-            :rtype: str
         """
         selected = self.subtable.getString("selected", None)
         if selected is None:
             selected = self.subtable.getString("default", None)
         return selected
 
-    def setSelected(self, selection):
+    def setSelected(self, selection: str) -> None:
         """
             Sets the active selection on the chooser
             
